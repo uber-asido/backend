46896838b62bb06d542ac900af4ac4fcaecc8eac
Arvydas Sidorenko <asido4@gmail.com>
2018-05-10 16:26:27 +0200

    Created solution and project structure.

---
+++ src/Uber.Module.Movie.Writer.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Uber.Module.Movie.Writer.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
