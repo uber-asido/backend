9c221d51769b8644371a14ca918aace1747d7fd5
Arvydas Sidorenko <asido4@gmail.com>
2018-05-13 13:58:17 +0200

    Created google geocode provider project.

---
+++ src/Uber.Module.Geocoding.Google/GoogleGeocodeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Uber.Module.Geocoding.Abstraction.Service;
+
+namespace Uber.Module.Geocoding.Google
+{
+    public class GoogleGeocodeProvider : IGeocodeProvider
+    {
+        public Task<Geocode> Geocode(string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
